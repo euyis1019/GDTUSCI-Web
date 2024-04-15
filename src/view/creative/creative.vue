390583e3cfb67fd092d5bd9bf30c72fa15607377
ding113 <xiaoding1103@gmail.com>
2024-04-15 19:44:43 +0800

    Refactor view components and remove unused code

--- src/view/creative/creative.vue
+++ src/view/creative/creative.vue
@@ -8,38 +8,32 @@
     <div class="insights">
         <div class="insightsItem">
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
-                <p class="insightsP"@click="$router.push('/creativeA')">Design Application of Concrete Materials Exploration and Practice
+                <h5 @click="$router.push('/creativeA')">材料的探索与实践_混凝土材料的设计应用</h5>
+                <p class="insightsP" @click="$router.push('/creativeA')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/creativeA"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
-                        srcset=""></router-link></div>
+            <div class="insightsImg"><router-link to="/creativeA"><img src="./images/banner.png" alt="" srcset=""></router-link></div>
         </div>
         <hr class="division-line">
         <div class="insightsItem">
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
-                <p class="insightsP"@click="$router.push('/creativeB')">Design Application of Concrete Materials Exploration and Practice
+                <h5 @click="$router.push('/creativeB')">材料的探索与实践_混凝土材料的设计应用</h5>
+                <p class="insightsP" @click="$router.push('/creativeB')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/creativeB"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
-                        srcset=""></router-link></div>
+            <div class="insightsImg"><router-link to="/creativeB"><img src="./images/banner.png" alt="" srcset=""></router-link></div>
         </div>
         <hr class="division-line">
         <div class="insightsItem">
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
-                <p class="insightsP"@click="$router.push('/creativeC')">Design Application of Concrete Materials Exploration and Practice
+                <h5 @click="$router.push('/creativeC')">材料的探索与实践_混凝土材料的设计应用</h5>
+                <p class="insightsP" @click="$router.push('/creativeC')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/creativeC"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
-                        srcset=""></router-link></div>
+            <div class="insightsImg"><router-link to="/creativeC"><img src="./images/banner.png" alt="" srcset=""></router-link></div>
         </div>
     </div>
     <div class="productBtn">
@@ -49,16 +43,7 @@
     </div>
 </template>
 
-<script setup>
-function changeTextColor(event, isHover) {
-    const insightsText = event.target.closest('.insightsItem').querySelector('.insightsText p');
-    if (isHover) {
-        insightsText.classList.add('redText');
-    } else {
-        insightsText.classList.remove('redText');
-    }
-}
-</script>
+
 
 
 <style>
--- src/view/dynamic/dynamic.vue
+++ src/view/dynamic/dynamic.vue
@@ -5,18 +5,15 @@
         <p>我们为企业创造富有凝聚力的品牌形象,帮助品牌与用户更好地连接,帮助品牌实现目标与价值.</p>
     </div>
 
-
-
     <div class="insights">
         <div class="insightsItem">
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
+                <h5 @click="$router.push('/dynamicA')">材料的探索与实践_混凝土材料的设计应用</h5>
                 <p class="insightsP" @click="$router.push('/dynamicA')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/dynamicA"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
+            <div class="insightsImg"><router-link to="/dynamicA"><img src="./images/banner.png" alt=""
                         srcset=""></router-link></div>
         </div>
         <div>
@@ -24,28 +21,25 @@
         </div>
         <div class="insightsItem">
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
+                <h5 @click="$router.push('/dynamicB')">材料的探索与实践_混凝土材料的设计应用</h5>
                 <p class="insightsP" @click="$router.push('/dynamicB')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/dynamicB"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
+            <div class="insightsImg"><router-link to="/dynamicB"><img src="./images/banner.png" alt=""
                         srcset=""></router-link></div>
         </div>
         <div>
             <hr class="division-line">
         </div>
         <div class="insightsItem">
-
             <div class="insightsText">
-                <h5>材料的探索与实践_混凝土材料的设计应用</h5>
+                <h5 @click="$router.push('/dynamicC')">材料的探索与实践_混凝土材料的设计应用</h5>
                 <p class="insightsP" @click="$router.push('/dynamicC')">Design Application of Concrete Materials Exploration and Practice
                     of Materials</p>
                 <h6>2024-01</h6>
             </div>
-            <div class="insightsImg"><router-link to="/dynamicC"><img @mouseover="changeTextColor($event, true)"
-                        @mouseout="changeTextColor($event, false)" src="./images/banner.png" alt=""
+            <div class="insightsImg"><router-link to="/dynamicC"><img src="./images/banner.png" alt=""
                         srcset=""></router-link></div>
         </div>
     </div>
@@ -58,16 +52,7 @@
     <div class="blankArea"></div>
 </template>
 
-<script setup>
-function changeTextColor(event, isHover) {
-    const insightsText = event.target.closest('.insightsItem').querySelector('.insightsText p');
-    if (isHover) {
-        insightsText.classList.add('redText');
-    } else {
-        insightsText.classList.remove('redText');
-    }
-}
-</script>
+
 <style>
 @import '@/assets/css/item.css';
 </style>
